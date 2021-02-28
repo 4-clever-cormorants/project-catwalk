1c28bec4578db0b84d3abe20da905159daefb257
Sam Berston <samuelberston@gmail.com>
2021-02-28 12:46:02 -0800

    dev environment set up

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, '/../client/dist')));
+
+const port = 3000;
+
+const logger = (req, res, next) => {
+  console.log(`Receiving request to ${req.url} with method ${req.method}`);
+  next();
+};
+
+app.get('/', logger);
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
