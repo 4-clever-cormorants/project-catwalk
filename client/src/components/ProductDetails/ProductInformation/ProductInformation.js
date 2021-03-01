e456ebd908a65b60221323e0c66475a66f86dbdc
Sam Berston <samuelberston@gmail.com>
2021-03-01 17:26:50 -0500

    title renders as part of ProductInformation subcomponent

--- client/src/components/ProductDetails/ProductInformation/ProductInformation.js
+++ client/src/components/ProductDetails/ProductInformation/ProductInformation.js
@@ -2,12 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import Category from './Category';
+import Title from './Title';
 
 const ProductInformation = ({ product }) => (
   <div className="productInformation">
     {console.log(product)}
     product information
     <Category category={product.category} />
+    <Title title={product.name} />
   </div>
 );
 
