6609f732be0b7ee73e0abb48d1028e45d894859c
Jeffrey Yeh <nightcrow33@gmail.com>
2021-03-02 01:24:38 -0800

    add css stylesheet

--- client/src/components/RelatedProducts/AddToOutfit.js
+++ client/src/components/RelatedProducts/AddToOutfit.js
@@ -4,7 +4,7 @@ import propTypes from 'prop-types';
 // TODO: add eventListener to add current product to state
 const AddToOutfit = ({ addToOutfitHandler }) => (
   // eslint-disable-next-line object-curly-spacing
-  <div className="addToOutfit" id="addToOutfit" style={{border: 'solid'}}>
+  <div className="addToOutfit" id="addToOutfit">
     AddToOutfit
     <button type="button" id="addToOutfitButton" onClick={addToOutfitHandler}>+</button>
   </div>
