9571a1481afa03cb7e46926c07d56be77405ffe2
Jeffrey Yeh <nightcrow33@gmail.com>
2021-03-02 15:59:12 -0800

    add favor icon component

---
+++ client/src/components/RelatedProducts/favorIcon.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import propTypes from 'prop-types';
+
+const Favor = () => (
+  // eslint-disable-next-line object-curly-spacing
+  <div>
+    icon
+  </div>
+);
+
+Favor.propTypes = {
+
+};
+
+export default Favor;
