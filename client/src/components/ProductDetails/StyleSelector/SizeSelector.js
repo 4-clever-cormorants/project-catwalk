40f6018cc80eb327b423652850ac038afffc3950
Sam Berston <samuelberston@gmail.com>
2021-03-01 20:57:53 -0500

    created SizeSelector to render in StyleSelector

---
+++ client/src/components/ProductDetails/StyleSelector/SizeSelector.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SizeSelector = () => (
+  <label>
+    size selector
+    <select name="size" value="hi">
+      <option>hi</option>
+    </select>
+  </label>
+);
+
+export default SizeSelector;
--- client/src/components/ProductDetails/StyleSelector/StyleSelector.js
+++ client/src/components/ProductDetails/StyleSelector/StyleSelector.js
@@ -2,11 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import StylesDisplay from './StylesDisplay';
+import SizeSelector from './SizeSelector';
 
 const StyleSelector = () => (
   <div className="styleSelector">
     style selector
     <StylesDisplay />
+    <SizeSelector />
   </div>
 );
 
