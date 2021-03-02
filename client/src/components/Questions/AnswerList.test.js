05d87f61552d57ae66caa0fcbe1f867ec248b62f
rfduong <ronnyduong@gmail.com>
2021-03-01 19:12:38 -0800

    Added more testing for answer and answerlist, deleted coverage

--- client/src/components/Questions/AnswerList.test.js
+++ client/src/components/Questions/AnswerList.test.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import AnswerList from './AnswerList';
+import Answer from './Answer';
+import dummyAnswers from './dummyAnswers';
 
 it('should exist', () => {
   const wrapper = mount(<AnswerList />);
   expect(wrapper.exists()).toBe(true);
+});
+
+it('should render answer components if there are answers', () => {
+  const wrapper = mount(<AnswerList answers={dummyAnswers.results} />);
+  const answer = wrapper.find(Answer);
+  expect(answer.exists()).toBe(true);
 });
