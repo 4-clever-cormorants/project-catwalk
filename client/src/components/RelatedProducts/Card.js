8bf4ea19275dfa2da188d20c2236b23cb5692880
Jeffrey Yeh <nightcrow33@gmail.com>
2021-03-01 16:26:46 -0800

    add more dummy data

--- client/src/components/RelatedProducts/Card.js
+++ client/src/components/RelatedProducts/Card.js
@@ -2,10 +2,14 @@ import React from 'react';
 import propTypes from 'prop-types';
 
 const Card = ({ item }) => (
-  <div className="card" id={item.id.toString()}>
+  // eslint-disable-next-line object-curly-spacing
+  <div className="card" id={item.id.toString()} style={{border: 'solid'}}>
     <h3>
       {item.name}
     </h3>
+    <h5>
+      {item.default_price}
+    </h5>
   </div>
 );
 
