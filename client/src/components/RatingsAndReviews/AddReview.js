78774e4910dbcb09c2c2ceb9c72f69b3b295908f
Moshfeq <moshfeqshahrani@gmail.com>
2021-03-02 10:50:30 -0800

    implemented input and character limits on data submission

--- client/src/components/RatingsAndReviews/AddReview.js
+++ client/src/components/RatingsAndReviews/AddReview.js
@@ -1,3 +1,12 @@
+/* eslint-disable react/jsx-props-no-multi-spaces */
+/* eslint-disable no-multi-spaces */
+/* eslint-disable react/jsx-wrap-multilines */
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable no-else-return */
+/* eslint-disable react/jsx-closing-tag-location */
+/* eslint-disable indent */
+/* eslint-disable react/jsx-indent */
+/* eslint-disable react/jsx-one-expression-per-line */
 /* eslint-disable react/no-unused-state */
 /* eslint-disable class-methods-use-this */
 import React from 'react';
@@ -6,7 +15,6 @@ class AddReview extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      clicked: false,
       recommend: '',
       rating: 0,
       reviewSummary: '',
@@ -14,61 +22,110 @@ class AddReview extends React.Component {
       email: '',
       nickname: '',
     }
-    this.onChangeValue = this.onChangeValue.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
-  onChangeValue(event) {
-    console.log(event.target.value);
+  handleChange(event) {
+    this.setState({ [event.target.name]: event.target.value })
+    console.log(event.target.name)
   }
+
+  handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  submitData(event) {
+    //if (this.state.checkout > 3) {
+    event.preventDefault();
+    console.log(this.state)
+    // axios.post('/basicInfo', {
+    //   data
+    // })
+    //   .then(function (response) {
+    //     console.log(response);
+    //   })
+    //   .catch(function (error) {
+    //     console.log(error);
+    //   })
+  }
+
 
   render() {
     return (
-      <div onChange={this.onChangeValue}>
-        <span>Enter your rating</span>
-        <button type="submit">Add New Review</button>
+      <form onSubmit={this.handleSubmit}>
+              <h2>Enter your ratings</h2>
+              <h4> Characteristics</h4>
+              <span>Size</span>
+              <br></br>
+              <input type="radio" value="1" name="size" />A size too small
+              <input type="radio" value="2" name="size" />1/2 a size too small
+              <input type="radio" value="3" name="size" />Perfect
+              <input type="radio" value="4" name="size" />1/2 a size too big
+              <input type="radio" value="5" name="size" />A size too wide
+              <br></br>
+              <span>Width  </span>
+              <br></br>
+              <input type="radio" value="1" name="width" />Too Narrow
+              <input type="radio" value="2" name="width" />Slightly Narrow
+              <input type="radio" value="3" name="width" />Perfect
+              <input type="radio" value="4" name="width" />Slightly Wide
+              <input type="radio" value="5" name="width" />Too Wide
+              <br></br>
+              <span>Comfort  </span>
+              <br></br>
+              <input type="radio" value="1" name="comfort" />Uncomfortable
+              <input type="radio" value="2" name="comfort" />Slightly Uncomfortable
+              <input type="radio" value="3" name="comfort" />OK
+              <input type="radio" value="4" name="comfort" />Comfortable
+              <input type="radio" value="5" name="comfort" />Perfect
+              <br></br>
+              <span>Quality  </span>
+              <br></br>
+              <input type="radio" value="1" name="quality" />Runs Short
+              <input type="radio" value="2" name="quality" />Runs Slightly Short
+              <input type="radio" value="3" name="quality" />Perfect
+              <input type="radio" value="4" name="quality" />Runs Slightly Long
+              <input type="radio" value="5" name="quality" />Runs Long
+              <br></br>
+              <span>Length  </span>
+              <br></br>
+              <input type="radio" value="1" name="quality" />Runs Tight
+              <input type="radio" value="2" name="quality" />Runs Slightly Tight
+              <input type="radio" value="3" name="quality" />Perfect
+              <input type="radio" value="4" name="quality" />Runs Slightly Long
+              <input type="radio" value="5" name="quality" />Runs Long
+              <br></br>
+              <span>
+                  <br></br>
+        <span>
+          Nickname :
+          <input name="nickname" type="text" placeholder="jackson11!" maxLength={60} value={this.state.nickname} onChange={this.handleChange} />
+        </span>
         <br></br>
-        <span> Characteristics</span>
         <br></br>
-        <span>Size<  /span>
-        <input type="radio" value="1" name="size" />A size too small
-        <input type="radio" value="2" name="size" />1/2 a size too small
-        <input type="radio" value="3" name="size" />Perfect
-        <input type="radio" value="4" name="size" />1/2 a size too big
-        <input type="radio" value="5" name="size" />A size too wide
+          Review Summary :
+          <textarea name="reviewSummary" type="text" value={this.state.reviewSummary} onChange={this.handleChange} />
+        </span>
         <br></br>
-        <span>Width  </span>
-        <input type="radio" value="1" name="width" />Too Narrow
-        <input type="radio" value="2" name="width" />Slightly Narrow
-        <input type="radio" value="3" name="width" />Perfect
-        <input type="radio" value="4" name="width" />Slightly Wide
-        <input type="radio" value="5" name="width" />Too Wide
         <br></br>
-        <span>Comfort  </span>
-        <input type="radio" value="1" name="comfort" />Uncomfortable
-        <input type="radio" value="2" name="comfort" />Slightly Uncomfortable
-        <input type="radio" value="3" name="comfort" />OK
-        <input type="radio" value="4" name="comfort" />Comfortable
-        <input type="radio" value="5" name="comfort" />Perfect
+        <span>
+          Review Body :
+          <textarea  name="reviewBody" type="text" placeholder="Why did you like the product or not?"  maxLength={1000} value={this.state.reviewBody} onChange={this.handleChange} />
+        </span>
         <br></br>
-        <span>Quality  </span>
-        <input type="radio" value="1" name="quality" />Runs Short
-        <input type="radio" value="2" name="quality" />Runs Slightly Short
-        <input type="radio" value="3" name="quality" />Perfect
-        <input type="radio" value="4" name="quality" />Runs Slightly Long
-        <input type="radio" value="5" name="quality" />Runs Long
         <br></br>
-        <span>Length  </span>
-        <input type="radio" value="1" name="quality" />Runs Tight
-        <input type="radio" value="2" name="quality" />Runs Slightly Tight
-        <input type="radio" value="3" name="quality" />Perfect
-        <input type="radio" value="4" name="quality" />Runs Slightly Long
-        <input type="radio" value="5" name="quality" />Runs Long
+        <span>
+          Email :
+          <input name="email" type="text" placeholder="jackson11@email.com@" maxLength={60}  value={this.state.email} onChange={this.handleChange} />
+        </span>
         <br></br>
-
-      </div>
+        <br></br>
+             <button type="submit" onClick={(event) => this.submitData(event)}>Add New Review</button>
+           </form>
     )
+    }
   }
-}
+
 
 export default AddReview;
 
--- client/src/components/RatingsAndReviews/RatingsAndReviews.js
+++ client/src/components/RatingsAndReviews/RatingsAndReviews.js
@@ -1,3 +1,5 @@
+/* eslint-disable import/extensions */
+/* eslint-disable react/no-unused-state */
 /* eslint-disable react/jsx-one-expression-per-line */
 /* eslint-disable react/destructuring-assignment */
 /* eslint-disable react/no-array-index-key */
@@ -12,6 +14,7 @@ class RatingsAndReviews extends React.Component {
     this.state = {
       reviews: [],
       numReviews: 0,
+      clicked: false,
     };
   }
 
@@ -19,19 +22,26 @@ class RatingsAndReviews extends React.Component {
     this.setState({
       reviews: DummyData,
       numReviews: DummyData.length,
-    });
+    })
   }
 
+  
+  //handleChange(event) {
+  //  this.setState({clicked: true})
+  //console.log(event.target.value);
+//}
+
   render() {
+
     return (
       <div className="Reviews">
         <h1>Ratings and Reviews</h1>
-        <h2>{this.state.numReviews} reviews</h2>
+        <h3>{this.state.numReviews} reviews</h3>
         {
           // eslint-disable-next-line react/destructuring-assignment
           // eslint-disable-next-line react/jsx-wrap-multilines
           this.state.reviews.map((i, index) => <div key={index}>
-            <p>{i.name}</p>
+            <p>{i.nickname}</p>
             <p>{i.date}</p>
             <p>{i.reviewBody}</p>
             <p>{i.response}</p>
@@ -43,6 +53,7 @@ class RatingsAndReviews extends React.Component {
         <AddReview />
       </div>
     )
+
   }
 }
 
