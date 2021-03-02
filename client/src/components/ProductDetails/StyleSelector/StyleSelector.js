69459af9bade5213582cc77fa12146d647d1468a
Sam Berston <samuelberston@gmail.com>
2021-03-01 20:45:27 -0500

    created StylesDisplay component and got to render within StylesSelector subcomponent

--- client/src/components/ProductDetails/StyleSelector/StyleSelector.js
+++ client/src/components/ProductDetails/StyleSelector/StyleSelector.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import StylesDisplay from './StylesDisplay';
+
 const StyleSelector = () => (
   <div className="styleSelector">
     style selector
+    <StylesDisplay />
   </div>
 );
 
---
+++ client/src/components/ProductDetails/StyleSelector/StylesDisplay.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const StylesDisplay = () => (
+  <div className="StylesDisplay">
+    styles display
+  </div>
+);
+
+export default StylesDisplay;
