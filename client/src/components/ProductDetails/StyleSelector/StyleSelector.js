9196ec6a74b12a25b79e70bc4c61f862dba3e04d
Sam Berston <samuelberston@gmail.com>
2021-03-01 22:35:48 -0500

    StyleSelector PropTypes - style object

--- client/src/components/ProductDetails/StyleSelector/StyleSelector.js
+++ client/src/components/ProductDetails/StyleSelector/StyleSelector.js
@@ -4,12 +4,27 @@ import PropTypes from 'prop-types';
 import StylesDisplay from './StylesDisplay';
 import SizeSelector from './SizeSelector';
 
-const StyleSelector = () => (
+// refactor to hold the state of the selected style and
+// then refactor the size selector and qty selector to reflect the options for that selected style
+
+const StyleSelector = ({ styles }) => (
   <div className="styleSelector">
     style selector
     <StylesDisplay />
     <SizeSelector />
   </div>
 );
+
+StyleSelector.propTypes = {
+  styles: PropTypes.arrayOf(PropTypes.shape({
+    style_id: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    original_price: PropTypes.string.isRequired,
+    sale_price: PropTypes.string.isRequired,
+    'default?': PropTypes.bool.isRequired,
+    photos: PropTypes.arrayOf(PropTypes.objectOf(PropTypes.string)).isRequired,
+    skus: PropTypes.objectOf(PropTypes.object).isRequired,
+  })).isRequired,
+};
 
 export default StyleSelector;
