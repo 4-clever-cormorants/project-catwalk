d051fb61dfa48e622303cd75c2d96ce02b9ca1fa
Jeffrey Yeh <nightcrow33@gmail.com>
2021-03-01 16:15:27 -0800

    add card component and change outfit list requirement

--- client/src/components/RelatedProducts/List.js
+++ client/src/components/RelatedProducts/List.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import propTypes from 'prop-types';
+import Card from './Card';
 
-const List = ({ className }) => (
+const List = ({ className, productsList }) => (
   <div className={className}>
-    List
+    {productsList.map((item) => (
+      <div key={item.id.toString()}>
+        <Card item={item} />
+      </div>
+
+    ))}
   </div>
 );
 
 List.propTypes = {
   className: propTypes.string.isRequired,
+  productsList: propTypes.arrayOf(propTypes.object).isRequired,
 };
 
 export default List;
