6adb381149501ee6196c59cac6ca5b884923d983
Jeffrey Yeh <nightcrow33@gmail.com>
2021-03-02 20:09:32 -0800

    update add to outfitlist method and create drop method

--- client/src/components/RelatedProducts/Drop.js
+++ client/src/components/RelatedProducts/Drop.js
@@ -1,15 +1,16 @@
 import React from 'react';
-// import propTypes from 'prop-types';
+import propTypes from 'prop-types';
 
-const Drop = () => (
+const Drop = ({ id, dropHandler }) => (
   // eslint-disable-next-line object-curly-spacing
-  <div className="actionIconDrop">
+  <div className="actionIconDrop" onClick={dropHandler.bind(this, id)} onKeyPress={dropHandler.bind(this, id)} role="button" tabIndex={0}>
     Drop
   </div>
 );
 
-// Drop.propTypes = {
-
-// };
+Drop.propTypes = {
+  dropHandler: propTypes.func.isRequired,
+  id: propTypes.number.isRequired,
+};
 
 export default Drop;
