716f5c7a5daf547d9f2fc642186caf9c08716868
Jeffrey Yeh <nightcrow33@gmail.com>
2021-03-02 16:37:59 -0800

    create new components to seperate related product list and own outfit list

---
+++ client/src/components/RelatedProducts/Drop.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import propTypes from 'prop-types';
+
+const Drop = () => (
+  // eslint-disable-next-line object-curly-spacing
+  <div className="actionIcon">
+    Drop
+  </div>
+);
+
+// Drop.propTypes = {
+
+// };
+
+export default Drop;
