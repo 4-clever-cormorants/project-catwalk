b356eef1d31465895573941bb9fe68cb4e279f2f
Jeffrey Yeh <nightcrow33@gmail.com>
2021-03-04 11:16:22 -0800

    also get average ratings in response data

--- server/routes/relatedRoutes.js
+++ server/routes/relatedRoutes.js
@@ -10,15 +10,30 @@ const headers = {
   },
 };
 
+const average = (ratings) => {
+  const rates = Object.keys(ratings);
+  let totolReviews = 0;
+  let totolRatings = 0;
+  if (rates.length === 0) { return null; }
+  for (let i = 0; i < rates.length; i += 1) {
+    totolRatings += (5 - i) * parseInt(ratings[rates[i]], 10);
+    totolReviews += parseInt(ratings[rates[i]], 10);
+  }
+  return totolRatings / totolReviews;
+};
+
 const requestProductInfo = (productId) => axios.get(`${url}products/${productId}`, headers);
+const requestProductReviewsMeta = (productId) => axios.get(`${url}reviews/meta?product_id=${productId}`, headers);
 const requestProductStyle = (productId) => axios.get(`${url}products/${productId}/styles`, headers);
 const requestData = (productId) => Promise.all([
   requestProductInfo(productId),
   requestProductStyle(productId),
+  requestProductReviewsMeta(productId),
 ]).then((results) => {
   const productInfo = results[0].data;
   productInfo.sale_price = results[1].data.results[0].sale_price;
   productInfo.thumbnail_url = results[1].data.results[0].photos[0].thumbnail_url;
+  productInfo.average_ratings = average(results[2].data.ratings);
   return productInfo;
 });
 
