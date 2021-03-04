3cfe72a80627e83da025f1f9bdaf319b1201bbfa
Sam Berston <samuelberston@gmail.com>
2021-03-04 12:45:29 -0500

    StylesDisplay.test.js

--- client/src/components/ProductDetails/StyleSelector/StylesDisplay.test.js
+++ client/src/components/ProductDetails/StyleSelector/StylesDisplay.test.js
@@ -9,7 +9,9 @@ import styles from '../stylesDummyData';
 test('StylesDisplay should render the styles of the given product', () => {
   const wrapper = mount(<StylesDisplay styles={styles.results} onClick={() => {}} />);
   const stylesDisplay = wrapper.find(StylesDisplay);
+  const len = wrapper.find('.stylesDisplay').children().filter('.style').length;
   const style = wrapper.find(Style);
   expect(stylesDisplay.exists()).toBe(true);
+  expect(len).toBeGreaterThan(1);
   expect(style.exists()).toBe(true);
 });
