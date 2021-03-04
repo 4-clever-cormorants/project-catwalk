c920ea69e14a959222897e68822e1216ceffc7cc
Jeffrey Yeh <nightcrow33@gmail.com>
2021-03-03 22:09:56 -0800

    get related products info from multiple apis

--- server/routes/relatedRoutes.js
+++ server/routes/relatedRoutes.js
@@ -10,13 +10,30 @@ const headers = {
   },
 };
 
+const requestProductInfo = (productId) => axios.get(`${url}products/${productId}`, headers);
+const requestProductStyle = (productId) => axios.get(`${url}products/${productId}/styles`, headers);
+const requestData = (productId) => Promise.all([
+  requestProductInfo(productId),
+  requestProductStyle(productId),
+]).then((results) => {
+  const productInfo = results[0].data;
+  productInfo.sale_price = results[1].data.results[0].sale_price;
+  productInfo.thumbnail_url = results[1].data.results[0].photos[0].thumbnail_url;
+  return productInfo;
+});
+
 router.get('/relatedProducts', (req, res) => {
   axios
     .get(`${url}products/${req.query.product_id}/related`, headers)
     .then((related) => {
-      res.send(related.data);
-      // for (let i = 0; i < related.data.length; i += 1) {
-      // }
+      const wait = [];
+      for (let i = 0; i < related.data.length; i += 1) {
+        wait.push(requestData(related.data[i]));
+      }
+      Promise.all(wait)
+        .then((dataList) => {
+          res.send(dataList);
+        });
     })
     .catch((error) => {
       res.status(500).send(error);
