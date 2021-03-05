4c6c77b96ebe6a25ee1442bb6355ef2d46de140d
Sam Berston <samuelberston@gmail.com>
2021-03-04 23:45:18 -0500

    ratingRoutes.test.js

---
+++ server/routes/ratingRoutes.test.js
@@ -0,0 +1,12 @@
+const supertest = require('supertest');
+const expServer = require('../server');
+
+test('GET /rating/data', () => {
+  supertest(expServer.app)
+    .get('/rating')
+    .expect(200)
+    .then((res) => {
+      expect(typeof res.data).toBe('object');
+    });
+  expServer.server.close();
+});
