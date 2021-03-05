5a0ef0b32cf41dda0a6969178a36d9958b114726
Sam Berston <samuelberston@gmail.com>
2021-03-05 15:22:20 -0500

    StyleSelector --> stateless functional component

--- client/src/components/ProductDetails/StyleSelector/StyleSelector.js
+++ client/src/components/ProductDetails/StyleSelector/StyleSelector.js
@@ -7,60 +7,25 @@ import QtySelector from './QtySelector';
 import AddToCart from './AddToCart';
 import Favorite from './Favorite';
 
-// refactor to hold the state of the selected style and
-// then refactor the size selector and qty selector to reflect the options for that selected style
-
-class StyleSelector extends React.Component {
-  constructor(props) {
-    super(props);
-    const { sku } = this.props;
-    this.state = {
-      sku,
-      cart: [],
-    };
-  }
-
-  // skuSelector(e) {
-  //   this.setState({
-  //     sku: e.target.value,
-  //   });
-  // }
-
-  // addToCart(e) {
-  //   e.preventDefault();
-  //   const { cart, sku } = this.state;
-  //   if (cart.indexOf(sku) === -1) {
-  //     this.setState({
-  //       cart: [...cart, sku],
-  //     });
-  //   }
-  // }
-
-  render() {
-    const {
-      styles, style, styleSelector, skuSelector, addToCart, sku,
-    } = this.props;
-    // let { sku } = this.state;
-    const { skus } = style;
-    // if (skus[sku] === undefined) {
-    //   sku
-    // }
-    const qty = skus[sku].quantity;
-    return (
-      <div className="styleSelector">
-        <StylesDisplay styles={styles} onClick={styleSelector} />
-        <div className="checkout">
-          <form onSubmit={addToCart} className="form">
-            <SizeSelector skus={skus} onChange={skuSelector} />
-            <QtySelector qty={qty} />
-            <AddToCart />
-            <Favorite />
-          </form>
-        </div>
+const StyleSelector = ({
+  styles, style, styleSelector, skuSelector, addToCart, sku,
+}) => {
+  const { skus } = style;
+  const qty = skus[sku].quantity;
+  return (
+    <div className="styleSelector">
+      <StylesDisplay styles={styles} onClick={styleSelector} />
+      <div className="checkout">
+        <form onSubmit={addToCart} className="form">
+          <SizeSelector skus={skus} onChange={skuSelector} />
+          <QtySelector qty={qty} />
+          <AddToCart />
+          <Favorite />
+        </form>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 StyleSelector.propTypes = {
   styles: PropTypes.arrayOf(PropTypes.shape({
@@ -74,6 +39,7 @@ StyleSelector.propTypes = {
   })).isRequired,
   styleSelector: PropTypes.func.isRequired,
   skuSelector: PropTypes.func.isRequired,
+  addToCart: PropTypes.func.isRequired,
   style: PropTypes.shape({
     style_id: PropTypes.number.isRequired,
     name: PropTypes.string.isRequired,
