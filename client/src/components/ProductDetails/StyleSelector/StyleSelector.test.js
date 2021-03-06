f7b2f671457af29d235dd24e3179d2c17c563213
Sam Berston <samuelberston@gmail.com>
2021-03-05 22:41:06 -0500

    StyleSelector.test.js

--- client/src/components/ProductDetails/StyleSelector/StyleSelector.test.js
+++ client/src/components/ProductDetails/StyleSelector/StyleSelector.test.js
@@ -26,29 +26,6 @@ test('StyleSelector should render all the subcomponents', () => {
   expect(stylesDisplay.exists()).toBe(true);
 });
 
-// it('should add a selected sku to the cart only once per sku', () => {
-//   function styleSelector(e) {
-//     const styleId = e.target.classList[0];
-//     this.setState({
-//       styleId,
-//     });
-//     styles.results.forEach((style) => {
-//       if (style.style_id === styleId) {
-//         this.setState({
-//           style,
-//         });
-//       }
-//     });
-//   }
-//   const wrapper = mount(<StyleSelector styles={styles.results} styleSelector={styleSelector} styleId="76285" style={styles.results[0]} defaultSku={Object.keys(styles.results[0].skus)[0]} />);
-//   const instance = wrapper.instance();
-//   const form = wrapper.find('.form');
-//   form.simulate('submit');
-//   expect(instance.state.cart.length).toBe(1);
-//   form.simulate('submit');
-//   expect(instance.state.cart.length).toBe(1);
-// });
-
 test('StylesSelector should render the styles of the given product', () => {
   const wrapper = mount(<StyleSelector styles={styles.results} styleSelector={() => {}} />);
   const stylesDisplay = wrapper.find(StyleSelector);
