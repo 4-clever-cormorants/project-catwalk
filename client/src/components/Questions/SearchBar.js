6ba186112e2de860436de55467e727228c826b2f
rfduong <ronnyduong@gmail.com>
2021-03-05 18:24:46 -0800

    change state of searchbar

--- client/src/components/Questions/SearchBar.js
+++ client/src/components/Questions/SearchBar.js
@@ -4,14 +4,23 @@ class SearchBar extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      searchTerm: '',
     };
+  }
+
+  handleChange(e) {
+    this.setState({ searchTerm: e.target.value }, () => {
+      const { searchTerm } = this.state;
+      if (searchTerm.length >= 3) {
+        console.log(searchTerm);
+      }
+    });
   }
 
   render() {
     return (
       <div className="searchBar">
-        <input type="text" placeholder="Have a question? Search for answers…" />
+        <input type="text" placeholder="Have a question? Search for answers…" onChange={this.handleChange.bind(this)} />
         <button type="button">Search</button>
       </div>
     );
