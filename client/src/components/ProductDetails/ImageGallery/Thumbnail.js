63157c54418313774b365b28614cf4a3d21c3bd3
Sam Berston <samuelberston@gmail.com>
2021-03-06 19:53:25 -0500

    style photos render in image gallery thumbnail view with radio button

--- client/src/components/ProductDetails/ImageGallery/Thumbnail.js
+++ client/src/components/ProductDetails/ImageGallery/Thumbnail.js
@@ -1,22 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Thumbnail = ({ style, styleSelector }) => (
-  <div className="thumbnail" id={style.style_id} onClick={styleSelector} >
-    <img src={style.photos[0].url} alt="thumbnailimage" className={style.style_id} />
+const Thumbnail = ({ url }) => (
+  <div className="thumbnail" id={url}>
+    <label>
+      <input type="radio" name="thumbnail" className="styleRadio" />
+      <img src={url} alt="thumbnailimage" />
+    </label>
   </div>
 );
 
 Thumbnail.propTypes = {
-  style: PropTypes.shape({
-    style_id: PropTypes.number.isRequired,
-    name: PropTypes.string.isRequired,
-    original_price: PropTypes.string.isRequired,
-    sale_price: PropTypes.string,
-    'default?': PropTypes.bool.isRequired,
-    photos: PropTypes.arrayOf(PropTypes.objectOf(PropTypes.string)).isRequired,
-    skus: PropTypes.objectOf(PropTypes.object).isRequired,
-  }).isRequired,
-  styleSelector: PropTypes.func.isRequired,
+  url: PropTypes.string.isRequired,
 };
 export default Thumbnail;
