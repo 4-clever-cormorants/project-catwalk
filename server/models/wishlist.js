c0faa7800c30dd675f654cf2c33e2a1fc20b202d
Jeffrey Yeh <nightcrow33@gmail.com>
2021-03-07 16:33:10 -0800

    add wishlist model and test

---
+++ server/models/wishlist.js
@@ -0,0 +1,44 @@
+const wishlist = {};
+let wishlistStore = [];
+
+/**
+ *
+ * @returns {Array} - contain a list of product_id in number type
+ */
+const getAll = () => wishlistStore;
+
+/**
+ *
+ * @param {number} id - current product_id
+ * @param {func} callback - error first callback
+ */
+const add = (id, callback) => {
+  if (id in wishlist) {
+    callback('already exist');
+  } else {
+    wishlist[id] = wishlistStore.length;
+    wishlistStore.push(id);
+    callback(null);
+  }
+};
+
+/**
+ *
+ * @param {number} id - product_id you want to drop
+ * @param {func} callback - error first callback
+ */
+const drop = (id, callback) => {
+  if (id in wishlist) {
+    wishlistStore = wishlistStore
+      .slice(0, wishlist[id])
+      .concat(wishlistStore.slice(wishlist[id] + 1));
+    delete wishlist[id];
+  }
+  callback(null);
+};
+
+module.exports = {
+  getAll,
+  add,
+  drop,
+};
---
+++ server/models/wishlist.test.js
@@ -0,0 +1,39 @@
+import dummy from '../../client/src/components/RelatedProducts/dummy_related';
+
+const wishlist = require('./wishlist');
+
+describe('Test the wishlist module', () => {
+  test('wishlist should be empty before add any items', () => {
+    const list = wishlist.getAll();
+    expect(list.length).toBe(0);
+  });
+
+  test('wishlist should able to add one item with add', () => {
+    wishlist.add(dummy.currentProduct.id, () => {});
+    const list = wishlist.getAll();
+    expect(list.length).toBe(1);
+    expect(list[0]).toEqual(dummy.currentProduct.id);
+  });
+
+  test('should only add the same item once', () => {
+    wishlist.add(dummy.currentProduct.id, () => {});
+    const list = wishlist.getAll();
+    expect(list.length).toBe(1);
+  });
+
+  test('should read all items with getAll with the same order when add', () => {
+    wishlist.add(dummy.relatedProducts[0].id, () => {});
+    wishlist.add(dummy.relatedProducts[1].id, () => {});
+    const list = wishlist.getAll();
+    expect(list.length).toBe(3);
+    expect(list[0]).toEqual(dummy.currentProduct.id);
+    expect(list[1]).toEqual(dummy.relatedProducts[0].id);
+    expect(list[2]).toEqual(dummy.relatedProducts[1].id);
+  });
+
+  test('should able to drop product by id', () => {
+    wishlist.drop(dummy.currentProduct.id, () => {});
+    const list = wishlist.getAll();
+    expect(list.length).toBe(2);
+  });
+});
