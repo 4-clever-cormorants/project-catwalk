95693c8ae05b5bf1b579cbcfe3deb0d3639f3e60
Sam Berston <samuelberston@gmail.com>
2021-03-08 14:40:34 -0800

    using ratings dummy data for tests

---
+++ client/src/components/ProductDetails/ratingsDummyData.js
@@ -0,0 +1,34 @@
+const ratings = {
+  product_id: 14034,
+  ratings: {
+    1: 4,
+    2: 3,
+    3: 8,
+    4: 4,
+    5: 6,
+  },
+  recommended: {
+    false: 5,
+    true: 20,
+  },
+  characteristics: {
+    Fit: {
+      id: 47019,
+      value: 2.7083333333333333,
+    },
+    Length: {
+      id: 47020,
+      value: 2.5833333333333333,
+    },
+    Comfort: {
+      id: 47021,
+      value: 2.9166666666666667,
+    },
+    Quality: {
+      id: 47022,
+      value: 2.7083333333333333,
+    },
+  },
+};
+
+export default ratings;
