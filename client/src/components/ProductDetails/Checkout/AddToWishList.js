56ee16d3bd1964e8bd4429edc60fb859cadb50d5
Sam Berston <samuelberston@gmail.com>
2021-03-10 11:03:58 -0800

    use React Hook to conditionally render addToWishList button and changed to check mark/added

--- client/src/components/ProductDetails/Checkout/AddToWishList.js
+++ client/src/components/ProductDetails/Checkout/AddToWishList.js
@@ -5,10 +5,13 @@ import PropTypes from 'prop-types';
 import css from './Checkout.css';
 
 const AddToWishList = ({ productId }) => {
+  const [added, add] = React.useState(false);
+
   const addToWishList = (id) => {
     axios.post(`/wishList/add?product_id=${id}`)
       .then((res) => {
         console.log(res);
+        add(true);
       })
       .catch((err) => {
         console.error(err);
@@ -18,9 +21,19 @@ const AddToWishList = ({ productId }) => {
   return (
     <label id="addToWishList" className={css.addToWishList}>
       <button id="addToWishListButton" type="button" className={css.addToWishListButton} onClick={(e) => { addToWishList(e.target.id); }}>
-        <span className="fa fa-plus" />
-        &nbsp;
-        <u id={productId}>Add to Wish List</u>
+        {added ? (
+          <div className="added">
+            <span className="fa fa-check" />
+            &nbsp;
+            <u id={productId}>Added to Wish List</u>
+          </div>
+        ) : (
+          <div className={css.add}>
+            <span className="fa fa-plus" />
+            &nbsp;
+            <u id={productId}>Add to Wish List</u>
+          </div>
+        )}
       </button>
     </label>
   );
