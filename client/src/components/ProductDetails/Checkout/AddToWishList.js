f74b5c21f7ee467de4b55cd56700ac021642597e
Sam Berston <samuelberston@gmail.com>
2021-03-10 10:56:30 -0800

    adds items to wishlist

--- client/src/components/ProductDetails/Checkout/AddToWishList.js
+++ client/src/components/ProductDetails/Checkout/AddToWishList.js
@@ -1,15 +1,33 @@
 import React from 'react';
+import axios from 'axios';
+import PropTypes from 'prop-types';
 
 import css from './Checkout.css';
 
-const AddToWishList = () => (
-  <label id="addToWishList" className={css.addToWishList}>
-    <button id="addToWishListButton" type="button" className={css.addToWishListButton}>
-      <span className="fa fa-plus" />
-      &nbsp;
-      <u>Add to Wish List</u>
-    </button>
-  </label>
-);
+const AddToWishList = ({ productId }) => {
+  const addToWishList = (id) => {
+    axios.post(`/wishList/add?product_id=${id}`)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
+  return (
+    <label id="addToWishList" className={css.addToWishList}>
+      <button id="addToWishListButton" type="button" className={css.addToWishListButton} onClick={(e) => { addToWishList(e.target.id); }}>
+        <span className="fa fa-plus" />
+        &nbsp;
+        <u id={productId}>Add to Wish List</u>
+      </button>
+    </label>
+  );
+};
+
+AddToWishList.propTypes = {
+  productId: PropTypes.number.isRequired,
+};
 
 export default AddToWishList;
