53c94e7a6c4247a1ce72d344e6f8c9c195bad181
Sam Berston <samuelberston@gmail.com>
2021-03-10 11:50:53 -0800

    default view modal

--- client/src/components/ProductDetails/ImageGallery/DefaultView.js
+++ client/src/components/ProductDetails/ImageGallery/DefaultView.js
@@ -2,27 +2,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import DefaultViewModal from './DefaultViewModal';
+
 import css from './ImageGallery.css';
 
 const DefaultView = ({
   id, max, leftClick, rightClick, url,
-}) => (
-  <div id="defaultView" className={css.DV}>
-    <div id="scrollLeft" className={css.left}>
-      {id > 0 ? (
-        <button id="defaultViewLeftButton" type="button" onClick={leftClick} onKeyPress={leftClick} className="fa fa-chevron-left" />
-      ) : ''}
+}) => {
+  const [imageClicked, clickImage] = React.useState(false);
+  function exitModal() {
+    clickImage(false);
+  }
+  return (
+    <div id="defaultView" className={css.DV}>
+      <div id="scrollLeft" className={css.left}>
+        {id > 0 ? (
+          <button id="defaultViewLeftButton" type="button" onClick={leftClick} onKeyPress={leftClick} className="fa fa-chevron-left" />
+        ) : ''}
+      </div>
+      <div id={url} className={css.defaultView} onClick={() => { clickImage(true); }}>
+        <img id="defaultImage" src={url} alt="" />
+      </div>
+      <div className={css.right}>
+        {id < max - 1 ? (
+          <button id="defaultViewRightButton" type="button" onClick={rightClick} onKeyPress={rightClick} className="fa fa-chevron-right" />
+        ) : ''}
+      </div>
+      <DefaultViewModal url={url} imageClicked={imageClicked} exitModal={exitModal} />
     </div>
-    <div id="defaultImageContainer" className={css.defaultView}>
-      <img id="defaultImage" src={url} alt="" />
-    </div>
-    <div className={css.right}>
-      {id < max - 1 ? (
-        <button id="defaultViewRightButton" type="button" onClick={rightClick} onKeyPress={rightClick} className="fa fa-chevron-right" />
-      ) : ''}
-    </div>
-  </div>
-);
+  );
+};
 
 DefaultView.propTypes = {
   id: PropTypes.number.isRequired,
