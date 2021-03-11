1a1f5885c06757eeeaa07bc9e209b075f8cdec5f
rfduong <ronnyduong@gmail.com>
2021-03-11 14:11:50 -0800

    Made footer a stateless component

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -2,39 +2,29 @@ import React from 'react';
 
 import css from './Footer.css';
 
-class Footer extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    };
-  }
-
-  render() {
-    return (
-      <div className={css.footer}>
-        <div className={css.footerContent}>
-          <div className={css.githubIcon}>
-            <a href="https://github.com/4-clever-cormorants/project-catwalk">
-              <i className="fa fa-github fa-2x" aria-hidden="true" />
-            </a>
-          </div>
-          <div className={`${css.topButtonContainer} topButtonContainer`}>
-            <button
-              className={`${css.topButton} topButton`}
-              type="button"
-              onClick={() => window.scrollTo({
-                top: 0,
-                behavior: 'smooth',
-              })}
-            >
-              <i className="fa fa-chevron-up" aria-hidden="true" />
-              <span>Top</span>
-            </button>
-          </div>
-        </div>
+const Footer = () => (
+  <div className={css.footer}>
+    <div className={css.footerContent}>
+      <div className={css.githubIcon}>
+        <a href="https://github.com/4-clever-cormorants/project-catwalk">
+          <i className="fa fa-github fa-2x" aria-hidden="true" />
+        </a>
       </div>
-    );
-  }
-}
+      <div className={`${css.topButtonContainer} topButtonContainer`}>
+        <button
+          className={`${css.topButton} topButton`}
+          type="button"
+          onClick={() => window.scrollTo({
+            top: 0,
+            behavior: 'smooth',
+          })}
+        >
+          <i className="fa fa-chevron-up" aria-hidden="true" />
+          <span>Top</span>
+        </button>
+      </div>
+    </div>
+  </div>
+);
 
 export default Footer;
