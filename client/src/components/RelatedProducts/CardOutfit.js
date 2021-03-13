26a4716e93f9a2537931aa22216f7d3b82429686
Jeffrey Yeh <nightcrow33@gmail.com>
2021-03-13 12:27:03 -0800

    error handle for img lost

--- client/src/components/RelatedProducts/CardOutfit.js
+++ client/src/components/RelatedProducts/CardOutfit.js
@@ -6,28 +6,42 @@ import Rating from './Rating';
 import Price from '../ProductDetails/ProductInformation/Price';
 import priceCss from './css/price.css';
 
-const CardOutfit = ({ item, dropHandler }) => (
-  <div className={`${style.card} ${style.outfitcards}`} id={`card${item.id}`}>
-    <Drop id={item.id} dropHandler={dropHandler} />
-    <div className={`cardImgContainer ${style.cardImgContainer}`}>
-      <img className={`cardImg ${style.cardImg}`} src={item.thumbnail_url} alt={item.name} loading="lazy" draggable="false" />
+const CardOutfit = ({ item, dropHandler }) => {
+  let image;
+  if (item.thumbnail_url) {
+    image = (
+      <div className={`cardImgContainer ${style.cardImgContainer}`}>
+        <img className={`cardImg ${style.cardImg}`} src={item.thumbnail_url} alt={item.name} draggable="false" loading="lazy" />
+      </div>
+    );
+  } else {
+    image = (
+      <div className={`cardImgLostContainer ${style.cardImgLostContainer}`}>
+        <div className={`cardImgLost ${style.cardImgLost}`}>Sorry Photo lost</div>
+      </div>
+    );
+  }
+  return (
+    <div className={`${style.card} ${style.outfitcards}`} id={`card${item.id}`}>
+      <Drop id={item.id} dropHandler={dropHandler} />
+      {image}
+      <a
+        className={`cardName ${style.cardName}`}
+        href={`/?product_id=${item.id}`}
+        draggable="false"
+        onClick={(e) => {
+          e.stopPropagation();
+        }}
+      >
+        <h3>
+          {item.name}
+        </h3>
+      </a>
+      <Price originalPrice={item.default_price} salePrice={item.sale_price} css={priceCss} />
+      <Rating rating={item.average_ratings} totalRatings={item.totalReviews} id={item.id} />
     </div>
-    <a
-      className={`cardName ${style.cardName}`}
-      href={`/?product_id=${item.id}`}
-      draggable="false"
-      onClick={(e) => {
-        e.stopPropagation();
-      }}
-    >
-      <h3>
-        {item.name}
-      </h3>
-    </a>
-    <Price originalPrice={item.default_price} salePrice={item.sale_price} css={priceCss} />
-    <Rating rating={item.average_ratings} totalRatings={item.totalReviews} id={item.id} />
-  </div>
-);
+  );
+};
 
 CardOutfit.propTypes = {
   item: propTypes.shape({
