5feac7080e3401f622103198f6ff5a639a3fd57c
Sam Berston <samuelberston@gmail.com>
2021-03-12 16:22:35 -0800

    refactored image gallery to react component

--- client/src/components/ProductDetails/ImageGallery/DefaultView.js
+++ client/src/components/ProductDetails/ImageGallery/DefaultView.js
@@ -6,32 +6,57 @@ import DefaultViewModal from './DefaultViewModal';
 
 import css from './ImageGallery.css';
 
-const DefaultView = ({
-  id, max, leftClick, rightClick, url,
-}) => {
-  const [imageClicked, clickImage] = React.useState(false);
-  function exitModal() {
-    clickImage(false);
+class DefaultView extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      clicked: false,
+    };
+    this.exitModal = this.exitModal.bind(this);
   }
 
-  return (
-    <div id="defaultView" className={css.DV}>
-      <div id="scrollLeft" className={css.left}>
-        {id > 0 ? (
-          <button id="defaultViewLeftButton" type="button" onClick={leftClick} onKeyPress={leftClick} className="fa fa-chevron-left" />
-        ) : ''}
+  exitModal() {
+    this.setState({
+      clicked: false,
+    });
+  }
+
+  render() {
+    const {
+      id, max, leftClick, rightClick, url,
+    } = this.props;
+    const { clicked } = this.state;
+    return (
+      <div id="defaultView" className={css.DV}>
+        <div id="scrollLeft" className={css.left}>
+          {id > 0 ? (
+            <button id="defaultViewLeftButton" type="button" onClick={leftClick} onKeyPress={leftClick} className="fa fa-chevron-left" />
+          ) : ''}
+        </div>
+        <div
+          id={url}
+          className={css.defaultView}
+          onClick={() => {
+            this.setState({
+              clicked: true,
+            });
+          }}
+          onFocus={() => {}}
+          onKeyPress={() => {}}
+          role="button"
+          tabIndex={0}
+        >
+          <img id="defaultImage" src={url} alt="" />
+        </div>
+        <div className={css.right}>
+          {id < max - 1 ? (
+            <button id="defaultViewRightButton" type="button" onClick={rightClick} onKeyPress={rightClick} className="fa fa-chevron-right" />
+          ) : ''}
+        </div>
+        <DefaultViewModal url={url} imageClicked={clicked} exitModal={this.exitModal} />
       </div>
-      <div id={url} className={css.defaultView} onClick={() => { clickImage(true); }}>
-        <img id="defaultImage" src={url} alt="" />
-      </div>
-      <div className={css.right}>
-        {id < max - 1 ? (
-          <button id="defaultViewRightButton" type="button" onClick={rightClick} onKeyPress={rightClick} className="fa fa-chevron-right" />
-        ) : ''}
-      </div>
-      <DefaultViewModal url={url} imageClicked={imageClicked} exitModal={exitModal} />
-    </div>
-  );
+    );
+  }
 };
 
 DefaultView.propTypes = {
