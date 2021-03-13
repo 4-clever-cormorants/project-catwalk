9007b035abbe54bf4d6a8d2ac2271f76c7862b9d
Jeffrey Yeh <nightcrow33@gmail.com>
2021-03-12 18:54:19 -0800

    add indicator navigate method

--- client/src/components/RelatedProducts/ScrollIndicator.test.js
+++ client/src/components/RelatedProducts/ScrollIndicator.test.js
@@ -4,11 +4,11 @@ import ScrollIndicator from './ScrollIndicator';
 
 describe('Test Related Product list component', () => {
   it('should render every card when in related list', () => {
-    const indicators = shallow(<ScrollIndicator scrollLength={4} listName="relatedList" />);
+    const indicators = shallow(<ScrollIndicator scrollLength={4} listName="relatedList" scrollToInd={() => {}} />);
     expect(indicators.find('.relatedListindicator').length).toEqual(4);
   });
   it('should render every card when in outfit list, but the fist and last one is hidden', () => {
-    const indicators = shallow(<ScrollIndicator scrollLength={4} listName="outfitList" />);
+    const indicators = shallow(<ScrollIndicator scrollLength={4} listName="outfitList" scrollToInd={() => {}} />);
     expect(indicators.find('.outfitListindicator').length).toEqual(4);
     expect(indicators.find('.hide').length).toEqual(2);
   });
